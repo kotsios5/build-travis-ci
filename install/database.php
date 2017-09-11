7b814d028b44d4c2b79d2e0571938278a0b3fa3b
kotsios5 <pantazis.constantinos@gmail.com>
2017-09-11 13:21:38 +0100

    test

--- install/database.php
+++ install/database.php
@@ -6,7 +6,7 @@ return array
         'connection' => array(
             'hostname'   => 'localhost',
             'username'   => 'root',
-            'password'   => '',
+            'password'   => '596782684667',
             'persistent' => FALSE,
             'database'   => 'openclassifieds',
             'variables'  => array('sql_mode'=>'')
