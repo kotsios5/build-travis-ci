73d7c847a870093812ac4c266332b8048089d261
kotsios5 <pantazis.constantinos@gmail.com>
2018-01-15 15:58:48 +0000

    test php7.2

--- php7-2.sh
+++ php7-2.sh
@@ -2,6 +2,7 @@ sudo apt-get install -y language-pack-en-base
 sudo LC_ALL=en_US.UTF-8 add-apt-repository ppa:ondrej/php -y
 sudo apt-get update
 sudo apt-get install php7.2
+sudo apt-get install libapache2-mod-php7..2
 # sudo apt-get install php7.2-curl -y
 
 # sudo apt-get install php7.2-apc -y
