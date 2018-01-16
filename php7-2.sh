cfdd0c416532d595aafeb40aa232256d77a2d467
kotsios5 <pantazis.constantinos@gmail.com>
2018-01-16 18:55:28 +0000

    test php7.2

--- php7-2.sh
+++ php7-2.sh
@@ -1,11 +1,11 @@
-sudo apt-get update
-sudo apt-get dist-upgrade -y
-sudo apt-get install libapache2-mod-php -y
-
 sudo apt-get install -y language-pack-en-base
 sudo LC_ALL=en_US.UTF-8 add-apt-repository ppa:ondrej/php -y
 sudo apt-get update
 sudo apt-get install php7.2
+
+sudo apt-get update
+sudo apt-get dist-upgrade -y
+
 sudo apt-get install php7.2-fpm -y
 sudo apt-get install libapache2-mod-php7.2 -y
 
