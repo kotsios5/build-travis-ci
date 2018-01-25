9045a9d4c3f61b8eb331801d5c3a9edb916f09b8
kotsios5 <pantazis.constantinos@gmail.com>
2018-01-25 15:02:39 +0000

    test php7.2

--- php7-2.sh
+++ php7-2.sh
@@ -61,7 +61,7 @@ php --version
 
 # sudo service apache2 reload
 
-sudo sed -i 's/short_open_tag = Off/short_open_tag = On/g' /etc/php/7.2/cli/php.ini
-sudo sed -i 's/short_open_tag = Off/short_open_tag = On/g' /etc/php/7.2/fpm/php.ini
-sudo sed -i 's/short_open_tag = Off/short_open_tag = On/g' /etc/php/7.2/apache2/php.ini
-sudo service apache2 reload
+# sudo sed -i 's/short_open_tag = Off/short_open_tag = On/g' /etc/php/7.2/cli/php.ini
+# sudo sed -i 's/short_open_tag = Off/short_open_tag = On/g' /etc/php/7.2/fpm/php.ini
+# sudo sed -i 's/short_open_tag = Off/short_open_tag = On/g' /etc/php/7.2/apache2/php.ini
+# sudo service apache2 reload
