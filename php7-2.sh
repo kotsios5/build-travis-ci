c157cf251ab950bcef24860cec6e8e7341c17c14
kotsios5 <pantazis.constantinos@gmail.com>
2018-01-25 16:27:02 +0000

    test php7.2

--- php7-2.sh
+++ php7-2.sh
@@ -1,5 +1,7 @@
+
+
 sudo apt-get install -y language-pack-en-base
-sudo LC_ALL=en_US.UTF-8 add-apt-repository ppa:ondrej/php -y
+sudo LC_ALL=en_US.UTF-8 add-apt-repository ppa:ondrej -y
 sudo apt-get update
 sudo apt-get install php7.2
 
@@ -8,22 +10,23 @@ sudo apt-get dist-upgrade -y
 
 sudo apt-get install php7.2-fpm -y
 sudo apt-get install php7.2-cli -y
+sudo apt-get install php7.2-tidy -y
 sudo apt-get install libapache2-mod-php7.2 -y
 
-# sudo apt-get install php7.2-curl -y
-# sudo apt-get install php7.2-apc -y
-# sudo apt-get install php7.2-gd -y
+sudo apt-get install php7.2-curl -y
+sudo apt-get install php7.2-apc -y
+sudo apt-get install php7.2-gd -y
+sudo apt-get install php7.2-mbstring -y
+sudo apt-get install php7.2-mysql -y
+sudo apt-get install php7.2-xml -y
+sudo apt-get install php7.2-zip -y
+sudo apt-get install php7.2-soap -y
 
 sudo a2enmod proxy_fcgi setenvif
 sudo service apache2 reload
 sudo a2enconf php7.2-fpm
 sudo service apache2 reload
 
-# sudo apt-get install php7.2-mbstring -y
-# sudo apt-get install php7.2-mysql -y
-# sudo apt-get install php7.2-xml -y
-# sudo apt-get install php7.2-zip -y
-# sudo apt-get install php7.2-soap -y
 
 php --version
 
