d71a9429bdd29161920b9704f778c55414ead103
kotsios5 <pantazis.constantinos@gmail.com>
2018-02-24 20:11:34 +0000

    test travis wth docker

--- install/install-yclas.sh
+++ install/install-yclas.sh
@@ -4,21 +4,20 @@
 # remove install/install.lock
 # Import database with admin user and 4 ads
 ls /var/www
-ls /var/www/html
-sudo cp /build/install/auth.php /var/www/yclas-test/oc/config/
-sudo cp /build/install/database.php /var/www/yclas-test/oc/config/
-sudo cp /build/install/robots /var/www/yclas-test/robots.txt
-sudo cp /build/install/sitemap /var/www/yclas-test/sitemap.xml
-sudo cp /build/install/htaccess-install /var/www/yclas-test/.htaccess
-sudo rm -rf /var/www/yclas-test/install/
-sudo rm /var/www/yclas-test/install-yclas.php
+sudo cp /build/install/auth.php /var/www/oc/config/
+sudo cp /build/install/database.php /var/www/oc/config/
+sudo cp /build/install/robots /var/www/robots.txt
+sudo cp /build/install/sitemap /var/www/sitemap.xml
+sudo cp /build/install/htaccess-install /var/www/.htaccess
+sudo rm -rf /var/www/install/
+sudo rm /var/www/install-yclas.php
 mysql -uroot openclassifieds < /build/install/openclassifieds.sql
 
-sudo cd /var/www/yclas-test/oc
+sudo cd /var/www/oc
 sudo wget https://codeception.com/codecept.phar
-sudo chmod -R 777 /var/www/yclas-test/oc/
-php /var/www/yclas-test/oc/codecept.phar run acceptance tests/acceptance/SetUsersPasswordsCept.php
-sudo chmod 777 /var/www/yclas-test/oc/config/database.php
-php /var/www/yclas-test/oc/codecept.phar run acceptance tests/acceptance/UpdateDBCept.php
-sudo chmod 755 /var/www/yclas-test/oc/config/database.php
-php /var/www/yclas-test/oc/codecept.phar run acceptance tests/acceptance/default
+sudo chmod -R 777 /var/www/oc/
+php /var/www/oc/codecept.phar run acceptance tests/acceptance/SetUsersPasswordsCept.php
+sudo chmod 777 /var/www/oc/config/database.php
+php /var/www/oc/codecept.phar run acceptance tests/acceptance/UpdateDBCept.php
+sudo chmod 755 /var/www/oc/config/database.php
+php /var/www/oc/codecept.phar run acceptance tests/acceptance/default
