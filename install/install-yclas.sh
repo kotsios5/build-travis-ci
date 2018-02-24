26c6c8a658e5ce366de4e69001befb3b18c50405
kotsios5 <pantazis.constantinos@gmail.com>
2018-02-24 17:38:42 +0000

    test travis with docker

--- install/install-yclas.sh
+++ install/install-yclas.sh
@@ -11,6 +11,13 @@ sudo cp /build/install/sitemap /var/www/yclas-test/sitemap.xml
 sudo cp /build/install/htaccess-install /var/www/yclas-test/.htaccess
 sudo rm -rf /var/www/yclas-test/install/
 sudo rm /var/www/yclas-test/install-yclas.php
-mysql -uroot openclassifieds < /var/www/yclas-test/build/install/openclassifieds.sql
+mysql -uroot openclassifieds < /build/install/openclassifieds.sql
 
-php -v
+sudo cd /var/www/yclas-test/oc
+sudo wget https://codeception.com/codecept.phar
+sudo chmod -R 777 /var/www/yclas-test/oc/
+php /var/www/yclas-test/oc/codecept.phar run acceptance tests/acceptance/SetUsersPasswordsCept.php
+sudo chmod 777 /var/www/yclas-test/oc/config/database.php
+php /var/www/yclas-test/oc/codecept.phar run acceptance tests/acceptance/UpdateDBCept.php
+sudo chmod 755 /var/www/yclas-test/oc/config/database.php
+php /var/www/yclas-test/oc/codecept.phar run acceptance tests/acceptance/default
