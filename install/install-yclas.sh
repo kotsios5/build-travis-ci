4dc54b1610952c2c45a565de8231beb0722a4321
kotsios5 <pantazis.constantinos@gmail.com>
2018-02-25 14:17:52 +0000

    test travis wth docker

--- install/install-yclas.sh
+++ install/install-yclas.sh
@@ -1,7 +1,6 @@
 # Start apache2, mysql, postfix
 sudo service apache2 start
 ls /var/lib/mysql
-# sudo service mysql start
 sudo service postfix start
 echo 'INSTALL.SH'
 
@@ -11,7 +10,7 @@ sudo a2ensite reoc.lo.conf
 sudo service apache2 reload
 
 sudo cp /etc/hosts /hosts.new
-sudo sed -i 's/localhost/reoc.lo/g' /etc/hosts.new
+sudo sed -i 's/localhost/reoc.lo/g' /hosts.new
 sudo cp -f /hosts.new /etc/hosts
 
 sudo service apache2 reload
