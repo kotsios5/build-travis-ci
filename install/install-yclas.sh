718d36a3ea6b87edb15a6697c3729b65cfe411b1
kotsios5 <pantazis.constantinos@gmail.com>
2018-03-03 22:30:13 +0000

    travis with docker

--- install/install-yclas.sh
+++ install/install-yclas.sh
@@ -21,7 +21,7 @@ sudo rm -rf /var/www/install/
 sudo rm /var/www/install-yclas.php
 mysql -uroot -p1234 openclassifieds < /build/install/openclassifieds.sql
 
-sudo chmod -R 775 /var/www/*
+sudo chmod -R 777 /var/www/*
 sudo chown -R www-data:www-data /var/www/*
 # allow tests update db
 sudo chmod 777 /var/www/oc/config/database.php
