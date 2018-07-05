2254088ab26c5c8966ebe0f02cd31a4cd8bc48b7
kotsios5 <pantazis.constantinos@gmail.com>
2018-07-05 20:03:20 +0100

    test

--- install/install-yclas.sh
+++ install/install-yclas.sh
@@ -10,18 +10,12 @@ sudo service apache2 restart
 # Move files that installation should create
 # remove install/install.lock
 # Import database with admin user and 4 ads
-sudo cp /build/install/auth.php /var/www/oc/config/
-sudo cp /build/install/database.php /var/www/oc/config/
-sudo cp /build/install/robots /var/www/robots.txt
-sudo cp /build/install/sitemap /var/www/sitemap.xml
-sudo cp /build/install/htaccess-install /var/www/.htaccess
-sudo rm -rf /var/www/install/
-sudo rm /var/www/install-yclas.php
-mysql -uroot -p1234 openclassifieds < /build/install/openclassifieds.sql
+sudo rm -rf /var/www/yclas-test/install/
+sudo rm /var/www/yclas-test/install-yclas.php
 
-sudo chmod -R 777 /var/www/*
+sudo chmod -R 777 /var/www/yclas-test/*
 # sudo chown -R www-data:www-data /var/www/*
 # allow tests update db
-sudo chmod 777 /var/www/oc/config/database.php
+sudo chmod 777 /var/www/yclas-test/oc/config/database.php
 
 
